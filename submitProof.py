f56c58b3a6088020679f4e8d55fa379159e0dda5
richak13 <rkhed@seas.upenn.edu>
2024-11-11 21:17:43 -0500

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -100,8 +100,8 @@ def send_signed_msg(proof, random_leaf):
     
     # Sign and send the transaction
     signed_tx = w3.eth.account.sign_transaction(transaction, acct.key)
-    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    
+
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)  # Corrected method name
     return w3.toHex(tx_hash)
 
 
